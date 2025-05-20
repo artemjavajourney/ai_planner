8ef3cd1e9f5f36b9c2ed298f3a3311c80c382840
Artem <av.10.000@yandex.ru>
2025-05-20 18:27:40 +0300

    git commit -m "Initial commit: базовая структура проекта"

---
+++ src/test/java/vasilev/ru/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package vasilev.ru.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
